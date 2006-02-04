cefd90634bba159bfec23cdf881b85a13f267b59
Andreas J. König <andreas.koenig.7os6vvqr@franz.ak.mind.de>
2006-02-04 21:23:02 +0000

    Import of ANDK/Perl-Repository-APC-1.186 from CPAN.
    gitpan-cpan-distribution: Perl-Repository-APC
    gitpan-cpan-version:      1.186
    gitpan-cpan-path:         ANDK/Perl-Repository-APC-1.186.tar.gz
    gitpan-cpan-author:       ANDK
    gitpan-cpan-maturity:     released

--- lib/Perl/Repository/APC.pm
+++ lib/Perl/Repository/APC.pm
@@ -4,8 +4,8 @@ use strict;
 use warnings;
 use File::Spec;
 
-my $Id = q$Id: APC.pm 178 2006-02-04 21:01:22Z k $;
-our $VERSION = sprintf "%.3f", 1 + substr(q$Rev: 178 $,4)/1000;
+my $Id = q$Id: APC.pm 186 2006-02-04 21:20:54Z k $;
+our $VERSION = sprintf "%.3f", 1 + substr(q$Rev: 186 $,4)/1000;
 
 sub new {
   unless (@_ == 2){
